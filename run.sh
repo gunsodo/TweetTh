6af3df29a2d8f211f1063c4235a06b98e7a6f4d8
Azuromalachite <austinwongsatit@gmail.com>
2019-06-25 05:48:35 -0700

    Edit path

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 scrapy crawl TweetScraper -a query="#RIPThailand" -a lang="th"
 
 # extract text and save to file
-python utils.py --file_dir /data/tweet --path extract.txt
+python utils.py --file_dir data/tweet --path extract.txt
