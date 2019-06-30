43c08ac57a914f7cacf6938ea2da0f301d9c9064
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2019-06-30 10:15:21 +0700

    Modified run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ scrapy crawl TweetScraper -a query="#RIPThailand" -a lang="th"
 
 # extract text and save to file
 python utils.py --file_dir data/tweet --path extract.txt
+
+# get the results
+python keywords.py extract.csv <N_WORDS>
